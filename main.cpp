0f39d1f3964c21354519be05b728a82814fe9e44
Anderson Sales <anderson_1004@hotmail.com>
2020-10-19 22:02:34 -0300

    finalizado com bugs

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ struct No{
     float *solucao;
     float funcao_objetivo;
     int capacidade_mochila;
-    list<No>arvore;
 };
 
 struct CUSTO_BENEFICIO{
@@ -42,48 +41,64 @@ void guloso(CUSTO_BENEFICIO *&item, int qtd_itens){
     }
 }
 
-void relaxacao_linear(No &no, CUSTO_BENEFICIO *item, int qtd_itens, int capacidade_mochila, int *peso_itens, int *valor_itens, int *variaveisFixadas){
+No relaxacao_linear(No no, CUSTO_BENEFICIO *item, int qtd_itens, int capacidade_mochila, int *peso_itens, int *valor_itens, int *variaveisFixadas){
+
+    if(no.capacidade_mochila < 0){
+        no.capacidade_mochila = -1;
+        return no;
+    }
+
     no.funcao_objetivo = 0;
     no.capacidade_mochila = capacidade_mochila;
     for (int i = 0; i < qtd_itens; i++){
         no.solucao[i] = 0;
     }
 
+    No copia;
+
+    copia.chave = no.chave;
+    copia.capacidade_mochila = no.capacidade_mochila;
+    copia.funcao_objetivo = no.funcao_objetivo;
+    copia.solucao = new float[qtd_itens];
+    for(int i=0;i<qtd_itens;i++){
+        copia.solucao[i] = no.solucao[i];
+    }
+
     for(int i = 0; i < qtd_itens; i++){
        if(variaveisFixadas[i] == 1){
-           no.funcao_objetivo += valor_itens[i];
-           no.capacidade_mochila -= peso_itens[i];
-           no.solucao[i] = 1;
+           copia.funcao_objetivo += valor_itens[i];
+           copia.capacidade_mochila -= peso_itens[i];
+           copia.solucao[i] = 1;
        }else if(variaveisFixadas[i] == 0){
-           no.solucao[i] = 0;
+           copia.solucao[i] = 0;
        }
-        
+
     }
     for(int i = 0; i < qtd_itens; i++){
-        if((peso_itens[item[i].chave] <= no.capacidade_mochila)&&(variaveisFixadas[item[i].chave] == -1)){
-            no.solucao[item[i].chave] = 1;
-            no.funcao_objetivo += valor_itens[item[i].chave];
-            no.capacidade_mochila -= peso_itens[item[i].chave];
-        }else if((no.capacidade_mochila > 0)&&(variaveisFixadas[item[i].chave] == -1)){
-            no.chave = item[i].chave + 1;
-            no.solucao[item[i].chave] = (float)no.capacidade_mochila/peso_itens[item[i].chave];
-            no.funcao_objetivo += ((float)no.capacidade_mochila/peso_itens[item[i].chave])*valor_itens[item[i].chave];
-            no.capacidade_mochila -= ((float)no.capacidade_mochila/peso_itens[item[i].chave])*peso_itens[item[i].chave];
+        if((peso_itens[item[i].chave] <= copia.capacidade_mochila)&&(variaveisFixadas[item[i].chave] == -1)){
+            copia.solucao[item[i].chave] = 1;
+            copia.funcao_objetivo += valor_itens[item[i].chave];
+            copia.capacidade_mochila -= peso_itens[item[i].chave];
+        }else if((copia.capacidade_mochila > 0)&&(variaveisFixadas[item[i].chave] == -1)){
+            copia.chave = item[i].chave + 1;
+            copia.solucao[item[i].chave] = (float)copia.capacidade_mochila/peso_itens[item[i].chave];
+            copia.funcao_objetivo += ((float)copia.capacidade_mochila/peso_itens[item[i].chave])*valor_itens[item[i].chave];
+            copia.capacidade_mochila -= ((float)copia.capacidade_mochila/peso_itens[item[i].chave])*peso_itens[item[i].chave];
             break;
         }
 
     }
-    if(no.capacidade_mochila < 0){
-        no.capacidade_mochila = -1;
+    if(copia.capacidade_mochila < 0){
+        copia.capacidade_mochila = -1;
     }
 
-    if(no.arvore.size()==0){
-        no.arvore.push_back(no);
-    }
-    cout << "---------------------------------------------" << endl;
-    cout << "solucao do no: " << no.solucao[0] << " " << no.solucao[1] << " " << no.solucao[2] << " " << no.solucao[3] << " " << no.solucao[4] << endl;
-    cout << "capacidade da mochila: " << no.capacidade_mochila << endl;
-    cout << "funcao objetivo: " << no.funcao_objetivo << endl;
+
+    //cout << "---------------------------------------------" << endl;
+    //cout << "solucao do no: " << copia.solucao[0] << " " << no.solucao[1] << " " << no.solucao[2] << " " << no.solucao[3] << " " << no.solucao[4] << endl;
+    //cout << "capacidade da mochila: " << copia.capacidade_mochila << endl;
+    //cout << "funcao objetivo: " << copia.funcao_objetivo << endl;
+
+    return copia;
 }
 
 int main()
@@ -153,101 +168,93 @@ int main()
     }
     cout << "\n" << endl;
 
-    relaxacao_linear(no,item,qtd_itens,capacidade_mochila,peso_item,valor_item,variaveisFixadas);
-    arvore.push_front(no);
+    No novoNo = relaxacao_linear(no,item,qtd_itens,capacidade_mochila,peso_item,valor_item,variaveisFixadas);
+    arvore.push_front(novoNo);
 
     //esboço de como ficará a descida.
     float melhorSolucaoInteira = 0; //melhor solução inteira - MSI do vídeo
     float melhorLimite = 0; //melhor limite - ML do vídeo
-    float gap = 0;//formula do gap -> (melhorLimite - MSI)/MSI 
+    float gap = 0;//formula do gap -> (melhorLimite - MSI)/MSI
     bool stop = true;
     int cont = 0;
     No noTemp;
     No noAux;
     int marcar_indice = -1; //se o indice passar do for com valor = -1 não existe variável fracionária
     list<No>aux;
+    int size;
+    int contadorDeRelaxacoes = 0;
 
-    while(stop){   
-        cout << "__________________________________________________" << endl;
+    while(stop){
+        cout << "#######################  " << cont << " iteracao  #####################"<< endl;
         cout << "tamanho da pilha: " << arvore.size() << endl;
-        cout << "variaveis fixadas: " << variaveisFixadas[0] << " " << variaveisFixadas[1] << " " << variaveisFixadas[2] << " " << variaveisFixadas[3] << " " << variaveisFixadas[4] << endl;
+        cout << "variaveis fixadas antes de retirar da pilha: " << variaveisFixadas[0] << " " << variaveisFixadas[1] << " " << variaveisFixadas[2] << " " << variaveisFixadas[3] << " " << variaveisFixadas[4] << endl;
         noTemp = arvore.front(); //variável no segura o primeiro item da pilha
-        cout << "variavel fracionaria na arvore: " << arvore.front().solucao[0] << endl;
-        cout << "variavel fracionaria na arvore: " << arvore.front().solucao[1] << endl;
-        cout << "variavel fracionaria na arvore: " << arvore.front().solucao[2] << endl;
-        cout << "variavel fracionaria na arvore: " << arvore.front().solucao[3] << endl;
-        cout << "variavel fracionaria na arvore: " << arvore.front().solucao[4] << endl;
         arvore.pop_front(); //remove o primeiro item da pilha
         for (int i = 0; i < qtd_itens; i++){//for verifica qual variável é fracionária e salva seu índice
             if((noTemp.solucao[i] > 0)&&(noTemp.solucao[i] < 1)){
                 marcar_indice = i;
-            }   
+            }
         }
-        cout << "pegou a FO: " << noTemp.funcao_objetivo << endl;
-        cout << "variavel fracionaria: " << marcar_indice << endl;
+        
         if(marcar_indice != -1){ //verifica se existe variável fracionária
+            cout << "********** existe variavel fracionaria ***********" << endl;
+            cout << "indice da variavel fracionaria: " << marcar_indice << endl;
+            cout << "solucao No: " << noTemp.solucao[0] << " " << noTemp.solucao[1] << " " << noTemp.solucao[2] << " " << noTemp.solucao[3] << " " << noTemp.solucao[4] << endl;
+            cout << "pegou a FO: " << noTemp.funcao_objetivo << endl;
             variaveisFixadas[marcar_indice] = 1; //fixa a varivel em true, se for para o lado esquerdo na descida muda-se para false
-            cout << "filho da direita: " << endl;           
-            relaxacao_linear(no, item, qtd_itens, capacidade_mochila, peso_item, valor_item, variaveisFixadas);//usar o marcar_indice para fixar a variável na relaxação
-            No filhoDireita = no; //a variável no quarda o local da memória na função (gambiarra)
+            No filhoDireita = relaxacao_linear(noTemp, item, qtd_itens, capacidade_mochila, peso_item, valor_item, variaveisFixadas);//usar o marcar_indice para fixar a variável na relaxação
+            //No filhoDireita  //a variável no quarda o local da memória na função (gambiarra)
+            cout << "*********  filho da direita  **********" << endl;
             cout << "FO: " << filhoDireita.funcao_objetivo << endl;
             cout << "variavel fracionaria: " << marcar_indice << endl;
             cout << "variaveis fixadas: " << variaveisFixadas[0] << " " << variaveisFixadas[1] << " " << variaveisFixadas[2] << " " << variaveisFixadas[3] << " " << variaveisFixadas[4] << endl;
-            
-            cout << "filho da esquerda: " << endl; 
+            cout << "capacidade da mochila: " << filhoDireita.capacidade_mochila << endl;
+            cout << "-----------------------------------------------" << endl;
+            cout << "***********  filho da esquerda  *********** " << endl;
             variaveisFixadas[marcar_indice] = 0;
-            relaxacao_linear(no, item, qtd_itens, capacidade_mochila, peso_item, valor_item, variaveisFixadas);//usar o marcar_indice para fixar a variável na relaxação            
-            No filhoEsquerda = no;//a variável no quarda o local da memória na função (gambiarra)
+            No filhoEsquerda = relaxacao_linear(noTemp, item, qtd_itens, capacidade_mochila, peso_item, valor_item, variaveisFixadas);//usar o marcar_indice para fixar a variável na relaxação
+            //No filhoEsquerda = no;//a variável no quarda o local da memória na função (gambiarra)
             cout << "FO: " << filhoEsquerda.funcao_objetivo << endl;
             cout << "variavel fracionaria: " << marcar_indice << endl;
             cout << "variaveis fixadas: " << variaveisFixadas[0] << " " << variaveisFixadas[1] << " " << variaveisFixadas[2] << " " << variaveisFixadas[3] << " " << variaveisFixadas[4] << endl;
-
+            cout << "capacidade da mochila: " << filhoEsquerda.capacidade_mochila << endl;
+            cout << "-----------------------------------------------" << endl;
+            contadorDeRelaxacoes += 2;
             if((filhoDireita.capacidade_mochila == -1)&&(filhoEsquerda.capacidade_mochila == -1)){
-                cout << "soluções invalidas para os filhos, iteração" << cont << endl;
+                cout << "%%%%%%  soluções invalidas para os filhos, iteração  %%%%%%%" << cont << endl;
             }else if(filhoEsquerda.capacidade_mochila == -1){
                 variaveisFixadas[marcar_indice] = 1;
                 arvore.push_front(filhoDireita);
-                cout << "filho da esquerda poda por inviabilidade" << endl;
+                cout << "%%%%%%  filho da esquerda poda por inviabilidade  %%%%%%%%" << endl;
             }else if(filhoDireita.capacidade_mochila == -1){
                 arvore.push_front(filhoEsquerda);
                 variaveisFixadas[marcar_indice] = 0;
-                cout << "filho da direita poda por inviabilidade" << endl;
+                cout << "%%%%%%%  filho da direita poda por inviabilidade  %%%%%%%%" << endl;
             }else if(filhoDireita.funcao_objetivo > filhoEsquerda.funcao_objetivo){ //verificação de qual é o melhor filho baseado no valor da função objetivo
                     arvore.push_front(filhoEsquerda); //desce pra direita
-                    cout << "AQUI" << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[0] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[1] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[2] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[3] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[4] << endl;
                     arvore.push_front(filhoDireita);
-                    cout << arvore.size() << endl;
                     variaveisFixadas[marcar_indice] = 1;
                     cout << "desceu pra direita" << endl;
-                    
                     cout << "variaveis fixadas: " << variaveisFixadas[0] << " " << variaveisFixadas[1] << " " << variaveisFixadas[2] << " " << variaveisFixadas[3] << " " << variaveisFixadas[4] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[0] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[1] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[2] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[3] << endl;
-                    cout << "variavel fracionaria na arvore: " << arvore.front().solucao[4] << endl;
                 }else{
                     arvore.push_front(filhoDireita); //desce pra esquerda
                     arvore.push_front(filhoEsquerda);
                     variaveisFixadas[marcar_indice] = 0;
                     cout << "desceu pra esquerda" << endl;
                     cout << "variaveis fixadas: " << variaveisFixadas[0] << " " << variaveisFixadas[1] << " " << variaveisFixadas[2] << " " << variaveisFixadas[3] << " " << variaveisFixadas[4] << endl;
-                }         
-            
+                }
+
         }else{ //guardar a melhor solução inteira.
             if(melhorSolucaoInteira < noTemp.funcao_objetivo){
                 melhorSolucaoInteira = noTemp.funcao_objetivo;
+                variaveisFixadas[marcar_indice] = -1;
+                cout << "%%%%%%%  poda por otimalidade  %%%%%%%%" << endl;
             }
-            int size = arvore.size();
+            size = arvore.size();
             for(int i = 0; i < size; i++){ //este for tira todos os itens da pilha para verificar qual o melhor limite
                 noAux = arvore.front(); //variável no segura o primeiro item da pilha
                 arvore.pop_front(); //remove o primeiro item da pilha
-                if(melhorLimite < noAux.funcao_objetivo){ 
+                if(melhorLimite < noAux.funcao_objetivo){
                     melhorLimite = noAux.funcao_objetivo; //guardando o maior limite
                 }
                 aux.push_front(noAux);
@@ -258,19 +265,30 @@ int main()
                 aux.pop_front();//voltando os nós pra pilha original
             }
 
-            gap = ((melhorLimite - melhorSolucaoInteira)/melhorSolucaoInteira); //cálculo do gap 
+            gap = ((melhorLimite - melhorSolucaoInteira)/melhorSolucaoInteira); //cálculo do gap
 
-            cout << "ML: " << melhorLimite << endl;
-            cout << "MSI: " << melhorSolucaoInteira << endl;
-            cout << "GAP: " << gap << endl;
+            
         }
 
-        if(cont == 3){stop = false;};
+        if(arvore.size() == 0){
+            cout << "solucao otima: " << melhorSolucaoInteira << endl;
+            cout << "quantidade de relaxacoes resolvidas: " << contadorDeRelaxacoes << endl;
+            cout << "GAP: " << gap << endl;
+            stop = false;
+        }
+
+        if(cont == 100){
+            cout << "ML: " << melhorLimite << endl;
+            cout << "MSI: " << melhorSolucaoInteira << endl;
+            cout << "Numero de nos abertos: " << arvore.size() << endl;
+            cout << "GAP: " << gap << endl;
+            cont = 0;
+        };
         cont++;
         marcar_indice = -1;
-        if(arvore.size() == 0){stop = false;};
+
     }
-    
+
 
     cout << endl;
     delete[]valor_item;
